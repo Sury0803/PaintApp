bd9016a676304de78c8cb96f84d0f6142f49ceb0
Suryansh Saxena <suryansh.saxena_cs18@gla.ac.in>
2021-11-12 16:58:45 +0530

    Update index.js

--- Paint/index.js
+++ Paint/index.js
@@ -1,15 +1,56 @@
 var features = document.querySelector('features');
+const draw = document.querySelector('#draw');
 
 const inc = document.querySelector('#inc');
 const dec = document.querySelector('#dec');
 const num = document.querySelector('#num');
 const mycolor = document.querySelector('#color');
 const reset = document.querySelector('#reset');
-
+const rect = document.querySelector('#rect')
 var surycanvas = document.querySelector('#surycanvas');
 const syc = surycanvas.getContext('2d');
 surycanvas.width = window.innerWidth/2;
 surycanvas.height = window.innerHeight/2;
+
+
+// window.addEventListener('load',drawing());
+
+function drawing(){
+    surycanvas.addEventListener('mousedown', (e)=>{
+        eventDown = true;
+        a = e.offsetX;
+        b = e.offsetY;
+        drawCircle(a,b);
+    });
+    
+    surycanvas.addEventListener('mouseup', (e)=>{
+        eventDown = false ;
+        a = undefined;
+        b = undefined;
+    })
+    
+    surycanvas.addEventListener('mousemove', (e)=>{
+        if(eventDown){
+            const a2 = e.offsetX;
+            const b2 = e.offsetY;
+            
+            drawCircle(a2,b2);
+            drawLine(a, b, a2, b2);
+            
+            a = a2;
+            b = b2;
+        }
+    })
+}
+
+
+draw.addEventListener('click',()=>{
+    drawing();   
+})
+
+rect.addEventListener('click',(e)=>{
+    drawRect();
+})
 
 let size = 10;
 let eventDown = false;
@@ -17,30 +58,42 @@ let color = 'darkblue';
 let a;
 let b;
 
-surycanvas.addEventListener('mousedown', (e)=>{
-    eventDown = true;
-    a = e.offsetX;
-    b = e.offsetY;
-});
 
-surycanvas.addEventListener('mouseup', (e)=>{
-    eventDown = false ;
-    a = undefined;
-    b = undefined;
+rect.addEventListener('click',(e)=>{
+    surycanvas.addEventListener('mousedown', (e)=>{
+        eventDown = true;
+        a = e.offsetX;
+        b = e.offsetY;
+    });
+    
+    surycanvas.addEventListener('mouseup', (e)=>{
+        eventDown = false ;
+        a = undefined;
+        b = undefined;
+    })
+    
+    surycanvas.addEventListener('mousemove', (e)=>{
+        if(eventDown){
+            const a2 = e.offsetX;
+            const b2 = e.offsetY;
+            drawRect(a,b,a2,b2);
+            return;
+            syc.clearRect(0,0,surycanvas.width,surycanvas.height);
+            a = a2;
+            b = b2;
+        }
+    })
 })
 
-surycanvas.addEventListener('mousemove', (e)=>{
-    if(eventDown){
-        const a2 = e.offsetX;
-        const b2 = e.offsetY;
-
-        drawCircle(a2,b2);
-        drawLine(a, b, a2, b2);
-
-        a = a2;
-        b = b2;
-    }
-})
+function drawRect(a,b,a2,b2){
+    console.log('rect');
+    var width = a2 - a;
+    var height = b2 - b;
+    syc.beginPath();
+    syc.strokeRect(a, b, width, height);
+    syc.strokeStyle = color;
+    syc.closePath();
+}
 
 function drawCircle(a,b){
     syc.beginPath();
@@ -54,7 +107,7 @@ function drawLine(a,b,a2,b2){
     syc.moveTo(a,b);
     syc.lineTo(a2,b2);
     syc.strokeStyle = color;
-    syc.lineWidth = num*2;
+    syc.lineWidth = size*2;
     syc.stroke();
 }
 
